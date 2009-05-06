e5b4bb11b12f8a8a48125cec38ecd4a81bd4d5cd
serge.che <serge.che@37911ef6-b259-11dd-81ae-51688d75609f>
2009-05-06 21:51:07 +0000

    [+] Added Coda plugin [+] Added "Format with Markdown" script for Aptana

--- aptana/Previous Edit Point.js
+++ aptana/Previous Edit Point.js
@@ -3,7 +3,6 @@
  * Kudos: Sergey Chikuyonok (http://chikuyonok.ru)
  * License: EPL 1.0
  * Key: M3+[
- * Listener: commandService().addExecutionListener(this);
  * DOM: http://download.eclipse.org/technology/dash/update/org.eclipse.eclipsemonkey.lang.javascript
  * 
  * @include "lib/core.js"
@@ -23,27 +22,3 @@ function main() {
 	if (new_point != -1) 
 		editor.currentOffset = new_point;
 }
- 
-function commandService(){
-	var commandServiceClass = Packages.org.eclipse.ui.commands.ICommandService;
-
-	// same as doing ICommandService.class
-    var commandService = Packages.org.eclipse.ui.PlatformUI.getWorkbench().getAdapter(commandServiceClass);
-    return commandService;
-}
-
-/**
- * Called before any/every command is executed, so we must filter on command ID
- */
-function preExecute(commandId, event) {
-	if (commandId == "com.aptana.ide.editors.views.actions.actionKeyCommand"){
-		main();
-    }
-}
-
-/* Add in all methods required by the interface, even if they are unused */
-function postExecuteSuccess(commandId, returnValue) {}
-
-function notHandled(commandId, exception) {}
-
-function postExecuteFailure(commandId, exception) {}
