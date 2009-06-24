7fdb08f3ea90c5e54fb7b60411b17eba912cbe99
serge.che <serge.che@37911ef6-b259-11dd-81ae-51688d75609f>
2009-06-24 22:52:55 +0000

    More unittests for Python Zen Coding and bugfixes. Looks like everything works fine

--- python/unittest/test_internals.py
+++ python/unittest/test_internals.py
@@ -18,6 +18,7 @@ class TestDeepKey(unittest.TestCase):
 	
 	def testExistedByList(self):
 		self.assertTrue(zen.has_deep_key(test_obj, ['key1', 'key2', 'key3']))
+		self.assertFalse(zen.has_deep_key(test_obj, ['key1', 'key2', 'key4']))
 		
 	def testExistedByString(self):
 		self.assertTrue(zen.has_deep_key(test_obj, 'key1.key2.key3'))
