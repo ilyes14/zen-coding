0aa5b8fe87b694af54e71d075c4ffb331e28dd52
serge.che <serge.che@37911ef6-b259-11dd-81ae-51688d75609f>
2009-10-30 17:50:25 +0000

    Correct zen settings parsing and overriding

--- python/unittest/test_expand.py
+++ python/unittest/test_expand.py
@@ -7,7 +7,7 @@ import unittest
 
 from zencoding import zen_core as zen, stparser
 
-zen_settings = stparser.get_settings()
+zen.update_settings(stparser.get_settings())
 
 def expandAbbr(abbr, doc_type='html', profile_name='plain'):
 	return zen.expand_abbreviation(abbr, doc_type, profile_name)
