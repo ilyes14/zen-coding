4aa97e566a5b35d60b2adfbf48733daaa60a57fc
serge.che <serge.che@37911ef6-b259-11dd-81ae-51688d75609f>
2009-12-15 10:37:40 +0000

    Working on Zen Coding for <textarea>

---
+++ plugins/textarea/actions.js
@@ -0,0 +1,46 @@
+/**
+ * Middleware layer that communicates between editor and Zen Coding.
+ * This layer describes all available Zen Coding actions, like 
+ * "Expand Abbreviation".
+ * @author Sergey Chikuyonok (serge.che@gmail.com)
+ * @link http://chikuyonok.ru
+ * 
+ * @include "editor.js"
+ */
+
+/**
+ * Search for abbreviation in editor from current caret position
+ * @param {editor} editor Editor instance
+ * @return {String|null}
+ */
+function findAbbreviation(editor) {
+	var range = editor.getSelectionRange();
+	if (range.start != range.end) {
+		// abbreviation is selected by user
+		return editor.getContent().substring(range.start, range.end);
+	}
+	
+	// search for new abbreviation from current caret position
+	var cur_line = editor.getCurrentLine();
+	return zen_coding.extractAbbreviation(editor.getContent().substring(cur_line.start, cur_line.end));
+}
+
+/**
+ * Find and expand abbreviation in current editor
+ * @param {editor} editor Editor instance
+ * @param {String} type Syntax type (html, css, etc.)
+ * @param {String} profile_name Output profile name (html, xml, xhtml)
+ */
+function expandAbbreviation(editor, type, profile_name) {
+	profile_name = profile_name || 'xhtml';
+	
+	var caret_pos = editor.getSelectionRange().start,
+		abbr,
+		content = '';
+		
+	if ( (abbr = findAbbreviation(editor)) ) {
+		content = zen_coding.expandAbbreviation(abbr, type, profile_name);
+		if (content)
+			editor.replaceContent(content, caret_pos + abbr.length, caret_pos);
+	}
+}
---
+++ plugins/textarea/editor.js
@@ -0,0 +1,198 @@
+/**
+ * High-level editor interface which communicates with other editor (like 
+ * TinyMCE, CKEditor, etc.) or browser.
+ * Before using any of editor's methods you should initialize it with
+ * <code>editor.setTarget(elem)</code> method and pass reference to 
+ * &lt;textarea&gt; element.
+ * @example
+ * var textarea = document.getElemenetsByTagName('textarea')[0];
+ * editor.setTarget(textarea);
+ * //now you are ready to use editor object
+ * editor.getSelectionRange() 
+ * 
+ * @author Sergey Chikuyonok (serge.che@gmail.com)
+ * @link http://chikuyonok.ru
+ */
+var editor = (function(){
+	/** @param {Element} Source element */
+	var target = null,
+		/** Textual placeholder that identifies cursor position in pasted text */
+		caret_placeholder = '~{%cursor%}~';
+	
+	/**
+	 * Returns content of current target element
+	 */
+	function getContent() {
+		return target.value || '';
+	}
+	
+	/**
+	 * Returns selection indexes from element
+	 */
+	function getSelectionRange() {
+		if ('selectionStart' in target) { // W3C's DOM
+			var length = target.selectionEnd - target.selectionStart;
+			return {
+				start: target.selectionStart, 
+				end: target.selectionEnd 
+			};
+		} else if (document.selection) { // IE
+			target.focus();
+	 
+			var range = document.selection.createRange();
+			
+			if (range === null) {
+				return {
+					start: 0, 
+					end: target.value.length
+				};
+			}
+	 
+			var re = target.createTextRange();
+			var rc = re.duplicate();
+			re.moveToBookmark(range.getBookmark());
+			rc.setEndPoint('EndToStart', re);
+	 
+			return {
+				start: rc.text.length, 
+				end: rc.text.length + range.text.length
+			};
+		} else {
+			return null;
+		}
+	}
+	
+	/**
+	 * Creates text selection on target element
+	 * @param {Number} start
+	 * @param {Number} end
+	 */
+	function createSelection(start, end) {
+		// W3C's DOM
+		if ('setSelectionRange' in target) {
+			target.setSelectionRange(start, end);
+		} else if ('createTextRange' in target) {
+			var t = target.createTextRange();
+			t.moveStart("character", start);
+			t.moveEnd("character", end - target.value.length);
+			t.select();
+		}
+	}
+	
+	/**
+	 * Find start and end index of text line for <code>from</code> index
+	 * @param {String} text 
+	 * @param {Number} from 
+	 */
+	function findNewlineBounds(text, from) {
+		var len = text.length,
+			start = 0,
+			end = len - 1;
+			
+		// search left
+		for (var i = from; i > 0; i--) {
+			var ch = text.charAt(i);
+			if (ch == '\n' || ch == '\r') {
+				start = i;
+				break;
+			}
+		}
+		// search right
+		for (var j = from + 1; j < len; j++) {
+			var ch = text.charAt(j);
+			if (ch == '\n' || ch == '\r') {
+				end = j;
+				break;
+			}
+		}
+		
+		return {start: start, end: end};
+	}
+	
+	/**
+	 * Returns current caret position
+	 */
+	function getCaretPos() {
+		var selection = getSelectionRange();
+		return selection ? selection.start : null;
+	}
+	
+	return {
+		setTarget: function(elem) {
+			target = elem;
+		},
+		
+		getSelectionRange: getSelectionRange,
+		createSelection: createSelection,
+		
+		/**
+		 * Returns current line's start and end indexes
+		 */
+		getCurrentLine: function() {
+			var caret_pos = getCaretPos(),
+				content = getContent();
+			if (caret_pos === null) return null;
+			
+			return findNewlineBounds(content, caret_pos);
+		},
+		
+		/**
+		 * Replace editor's content or it's part (from <code>start</code> to 
+		 * <code>end</code> index). If <code>value</code> contains 
+		 * <code>caret_placeholder</code>, the editor will put caret into 
+		 * this position. If you skip <code>start</code> and <code>end</code>
+		 * arguments, the whole target's content will be replaced with 
+		 * <code>value</code>. 
+		 * 
+		 * If you pass <code>start</code> argument only,
+		 * the <code>value</code> will be placed at <code>start</code> string 
+		 * index of current content. 
+		 * 
+		 * If you pass <code>start</code> and <code>end</code> arguments,
+		 * the corresponding substring of current target's content will be 
+		 * replaced with <code>value</code>. 
+		 * @param {String} value Content you want to paste
+		 * @param {Number} [start] Start index of editor's content
+		 * @param {Number} [end] End index of editor's content
+		 */
+		replaceContent: function(value, start, end) {
+			var content = getContent(),
+				caret_pos = getCaretPos(),
+				has_start = typeof(start) !== 'undefined',
+				has_end = typeof(end) !== 'undefined';
+				
+			// find new caret position
+			var new_pos = value.indexOf(caret_placeholder);
+			if (new_pos != -1) {
+				caret_pos = (start || 0) + new_pos;
+				value = value.replace(caret_placeholder, '');
+			}
+			
+			try {
+				if (has_start && has_end) {
+					content = content.substring(0, start) + value + content.substring(end);
+				} else if (has_start) {
+					content = content.substring(0, start) + value;
+				}
+				
+				target.value = content;
+				createSelection(caret_pos, caret_pos);
+			} catch(e){}
+		},
+		
+		/**
+		 * Returns cursor placeholder value
+		 * @return {String}
+		 */
+		getCaretPlaceholder: function() {
+			return caret_placeholder;
+		},
+		
+		/**
+		 * Returns editor's content
+		 * @return {String}
+		 */
+		getContent: getContent
+	}
+})();
+ 
