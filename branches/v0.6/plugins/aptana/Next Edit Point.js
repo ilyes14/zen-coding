a544b9237a70a7d7e244988920d73e22951e82c3
serge.che <serge.che@37911ef6-b259-11dd-81ae-51688d75609f>
2010-01-14 21:40:42 +0000

    Changed shortcuts for Aptana actions

--- branches/v0.6/plugins/aptana/Next Edit Point.js
+++ branches/v0.6/plugins/aptana/Next Edit Point.js
@@ -2,7 +2,7 @@
  * Menu: Zen Coding > Next Edit Point
  * Kudos: Sergey Chikuyonok (http://chikuyonok.ru)
  * License: EPL 1.0
- * Key: M3+]
+ * Key: M3+M4+]
  * DOM: http://download.eclipse.org/technology/dash/update/org.eclipse.eclipsemonkey.lang.javascript
  * 
  * @include "/EclipseMonkey/scripts/monkey-doc.js"
--- branches/v0.6/plugins/aptana/Previous Edit Point.js
+++ branches/v0.6/plugins/aptana/Previous Edit Point.js
@@ -2,14 +2,14 @@
  * Menu: Zen Coding > Previuos Edit Point
  * Kudos: Sergey Chikuyonok (http://chikuyonok.ru)
  * License: EPL 1.0
- * Key: M3+[
+ * Key: M3+M4+[
  * DOM: http://download.eclipse.org/technology/dash/update/org.eclipse.eclipsemonkey.lang.javascript
  * 
  * @include "lib/zen_eclipse.js"
  * @include "/EclipseMonkey/scripts/monkey-doc.js"
  */
 
-include('lib/zen_eclipse.js');
+include('zencoding.js');
 
 function main() {
 	zen_editor.setContext(editors.activeEditor);
