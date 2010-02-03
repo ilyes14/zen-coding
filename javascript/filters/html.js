8285b5ced4dc61ac8e797df14860cbffee4a7898
serge.che <serge.che@37911ef6-b259-11dd-81ae-51688d75609f>
2010-02-03 11:36:24 +0000

    Moved formatting to separate filter

--- javascript/filters/html.js
+++ javascript/filters/html.js
@@ -8,18 +8,6 @@
 (function(){
 	var child_token = '${child}';
 	
-	function getNewline() {
-		return zen_coding.getNewline();
-	}
-	
-	function getIndentation() {
-		return zen_coding.getVariable('indentation');
-	}
-	
-	function allowNewline(tag, profile) {
-		return (profile.tag_nl === true || (profile.tag_nl == 'decide' && tag.isBlock()));
-	}
-	
 	/**
 	 * Creates HTML attributes string from tag according to profile settings
 	 * @param {SimpleTag} tag
@@ -54,28 +42,13 @@
 			// snippet wasn't found, process it as tag
 			return processTag(item, profile, level);
 			
-		var parts = data.split(child_token);
-		item.start = parts[0] || '';
-		item.end = parts[1] || '';
-		
-		var padding = (item.parent) 
-			? item.parent.padding
-			: zen_coding.repeatString(getIndentation(), level);
-		
-		if (item.parent) {
-			item.start = getNewline() + padding + zen_coding.padString(item.start, padding);
-			item.end = zen_coding.padString(item.end, padding);
-		}
-		
-		// adjust item formatting according to last line of <code>start</code> property
-		var lines = zen_coding.splitByLines(item.start),
-			padding_delta = getIndentation();
-		if (lines > 1) {
-			var m = lines[lines.length - 1].match(/^(\s+)/);
-			if (m)
-				padding_delta = m[1];
-		}
-		item.padding = padding + padding_delta;
+		var parts = data.split(child_token),
+			start = parts[0] || '',
+			end = parts[1] || '',
+			padding = item.parent ? item.parent.padding : '';
+			
+		item.start = item.start.replace('%s', zen_coding.padString(start, padding));
+		item.end = item.end.replace('%s', zen_coding.padString(end, padding));
 		
 		return item;
 	}
@@ -104,9 +77,10 @@
 			content = '', 
 			cursor = profile.place_cursor ? zen_coding.getCaretPlaceholder() : '',
 			self_closing = '',
-			is_unary = (item.isUnary() && !item.children.length);
+			is_unary = (item.isUnary() && !item.children.length),
+			start= '',
+			end = '';
 			
-
 		if (profile.self_closing_tag == 'xhtml')
 			self_closing = ' /';
 		else if (profile.self_closing_tag === true)
@@ -115,36 +89,19 @@
 		// define opening and closing tags
 		var tag_name = (profile.tag_case == 'upper') ? item.name.toUpperCase() : item.name.toLowerCase();
 		if (is_unary) {
-			item.start = '<' + tag_name + attrs + self_closing + '>';
+			start = '<' + tag_name + attrs + self_closing + '>';
+			item.end = '';
 		} else {
-			item.start = '<' + tag_name + attrs + '>';
-			item.end = '</' + tag_name + '>';
-		}
-		
-		// formatting output
-		if (profile.tag_nl !== false) {
-			var padding = (item.parent) 
-				? item.parent.padding
-				: zen_coding.repeatString(getIndentation(), level);
-			
-			if ((item.isBlock() && item.parent) || profile.tag_nl === true) {
-				// formatting block-level elements
-				
-				// snippet children should take different formatting
-				if (!item.parent || item.parent.type != 'snippet')
-					item.start = getNewline() + padding + item.start;
-					
-				if (item.hasBlockChildren())
-					item.end = getNewline() + padding + item.end;
-			} else if (item.isInline() && hasBlockSibling(item)) {
-				item.start = getNewline() + padding + item.start;
-			}
-			
-			item.padding = padding + getIndentation();
+			start = '<' + tag_name + attrs + '>';
+			end = '</' + tag_name + '>';
 		}
 		
 		if (!item.children.length && !is_unary)
-			item.start += cursor;
+			start += cursor;
+			
+		item.start = item.start.replace('%s', start);
+		item.end = item.end.replace('%s', end);
+		
 		
 		return item;
 	}
@@ -157,14 +114,16 @@
 	 */
 	function process(tree, profile, level) {
 		level = level || 0;
+		if (level == 0)
+			tree = zen_coding.runFilters(tree, profile, '_format');
 		
 		for (var i = 0, il = tree.children.length; i < il; i++) {
 			/** @type {SimpleTag} */
+	
 			var item = tree.children[i];
-			if (item.type == 'tag')
-				item = processTag(item, profile, level);
-			
-			item = (item.type == 'tag') ? processTag(item, profile, level) : processSnippet(item, profile, level);
+			item = (item.type == 'tag') 
+				? processTag(item, profile, level) 
+				: processSnippet(item, profile, level);
 			
 			item.start = item.start.replace(/\$/g, i + 1);
 			process(item, profile, level + 1);
